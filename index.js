978065edfeaddb1345e833c8401fc5168a6720d8
James Connelly <jamesc1992@hotmail.com>
2015-08-09 22:17:14 +0100

    code cleanup
    removed the comments at the bottom. used _ prefix to denote private
    internal methods/properties. re-arranged the order of methods with
    public methods at the top. added comments to the public methods.

--- index.js
+++ index.js
@@ -1,59 +1,77 @@
 var SERVER = "http://localhost:8080/"
 
 var iRacingTelemetry = function() {
-	this.RequestRepeatRate = 100;
-	this.Server = "http://localhost";
-	this.Port = 8080;
-	this.currentTelemetryData = undefined;
-	this.OnUpdate = undefined;
+	this._requestRepeatRate = 100;
+	this._server = "http://localhost";
+	this._port = 8080;
+	this._currentTelemetryData = undefined;
+	this._onUpdate = undefined;
+	this._requestTimeout = undefined;
+	this._running = false;
 };
 
+// set the rate at which we poll for telemetry data in milliseconds
 iRacingTelemetry.prototype.SetRequestRepeatRate = function(requestRepeatRate) {
-	this.RequestRepeatRate = requestRepeatRate;
+	this._requestRepeatRate = requestRepeatRate;
 };
 
+// set the url of the telemetry server
 iRacingTelemetry.prototype.SetServer = function(server) {
-	this.Server = server;
+	this._server = server;
 };
 
+// set the port number of the telemetry server
 iRacingTelemetry.prototype.SetPort = function(port) {
-	this.Port = port;
+	this._port = port;
 };
 
+// register a callback function to be called whenever new telemetry data has been received
 iRacingTelemetry.prototype.RegisterUpdateCallback = function(callback) {
-	this.OnUpdate = callback;
+	this._onUpdate = callback;
 };
 
-iRacingTelemetry.prototype.requestData = function() {
-	var url = this.Server + ":" + this.Port.toString();
+// begin polling for telemetry data
+iRacingTelemetry.prototype.Start = function(data) {
+	this._running = true;
+	this._requestData();
+};
+
+// stop polling for telemetry data
+iRacingTelemetry.prototype.Stop = function(data) {
+	this._running = false;
+	if (this._requestTimeout) {
+		clearTimeout(this._requestTimeout);
+	}
+	this._requestData();
+};
+
+iRacingTelemetry.prototype._requestData = function() {
+	var url = this._server + ":" + this._port.toString();
 
 	var xmlhttp = new XMLHttpRequest();
 	xmlhttp.open("GET", encodeURI(url));
 
 	xmlhttp.onreadystatechange=function() {
 		if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-			this.onDataRetrieved(xmlhttp.responseText);
+			this._onDataRetrieved(xmlhttp.responseText);
 		}
 	}.bind(this);
 
 	xmlhttp.send();
 };
 
-iRacingTelemetry.prototype.onDataRetrieved = function(data) {
+iRacingTelemetry.prototype._onDataRetrieved = function(data) {
 	var jsonData = JSON.parse(data);
-	this.currentTelemetryData = jsonData;
-	if (this.OnUpdate) {
-		this.OnUpdate(this.currentTelemetryData);
+	this._currentTelemetryData = jsonData;
+	if (this._onUpdate) {
+		this._onUpdate(this._currentTelemetryData);
 	}
-	setTimeout(this.requestData.bind(this), this.RequestRepeatRate);
+	if (this._running) {
+		this._requestTimeout = setTimeout(this._requestData.bind(this), this._requestRepeatRate);
+	}
 };
 
-iRacingTelemetry.prototype.Start = function(data) {
-	this.requestData();
-};
 
-
-// if the module has no dependencies, the above pattern can be simplified to
 (function (root, factory) {
     if (typeof define === 'function' && define.amd) {
         // AMD. Register as an anonymous module.
@@ -70,14 +88,3 @@ iRacingTelemetry.prototype.Start = function(data) {
 }(this, function () {
     return iRacingTelemetry;
 }));
-
-
-// var onNewData = function(data) {
-// 	console.log(data);
-// };
-
-// var telem = new iRacingTelemetry();
-
-// telem.SetRequestRepeatRate(1000/20);
-// telem.RegisterUpdateCallback(onNewData);
-// telem.Start();
